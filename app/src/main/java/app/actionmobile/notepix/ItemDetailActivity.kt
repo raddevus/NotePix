3539c83046c05aa84025a8f4b3b63313c12422bd
raddeuvs <1radeutsch1@gmail.com>
2019-08-22 18:00:06 -0400

    Changed fab button icon to camera icon.

--- app/src/main/java/app/actionmobile/notepix/ItemDetailActivity.kt
+++ app/src/main/java/app/actionmobile/notepix/ItemDetailActivity.kt
@@ -20,7 +20,7 @@ class ItemDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_item_detail)
         setSupportActionBar(detail_toolbar)
 
-        fab.setOnClickListener { view ->
+        takePic.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
